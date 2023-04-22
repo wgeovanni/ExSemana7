6ba6a3d31d29601156d8ad335137425af996a39a
wgeovanni <willyangeo@gmail.com>
2023-04-22 00:02:18 -0300

    Exercicio semana 7

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Header } from "./components/Header";
+
+export const App = () => {
+  return (
+    <div>
+      <Header titulo="Negociações" />
+      <div className="container">
+        {/* OS COMPONENTES SERÃO ENCAIXADOS AQUI! */}
+      </div>
+    </div>
+  );
+};
